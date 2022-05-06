4c7a50dc1ca86eab1cfdabba9842beb9c017cbd8
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-05-06 14:33:57 -0300

    Terminado cubos

---
+++ Assets/Everything/Button_Behavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Behavior : MonoBehaviour
+{
+    public void ShowMessageInConsole()
+    {
+        Debug.Log("Hello World");
+    }
+}
---
+++ Assets/Everything/Instantiator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    // Start is called before the first frame update
+  
+    public void InstiantateCube()
+    {
+        int counter = 0;
+        while (counter < 3)
+        {
+            Instantiate(objectToClone);
+            counter++;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
